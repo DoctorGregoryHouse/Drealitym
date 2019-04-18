5b16d8777645047cfd40c38b9cbb3a5232f05f4f
DoctorGregoryHouse <dev.rkleinert@gmail.com>
2019-04-18 21:23:36 +0200

    Added Fragment layout file

--- app/src/main/java/com/example/ruben/drealitym/uiclasses/FragmentReminderPicker.java
+++ app/src/main/java/com/example/ruben/drealitym/uiclasses/FragmentReminderPicker.java
@@ -5,19 +5,56 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.example.ruben.drealitym.R;
 
 
 public class FragmentReminderPicker extends DialogFragment {
 
+
+    ToggleButton tb;
+
+    //this method initializes the views.
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+                return  inflater.inflate(R.layout.fragment_add_reminders, container, false );
+                
 
-
-        return  inflater.inflate(R.layout.fragment_add_reminders, container, false );
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+       tb = view.findViewById(R.id.reminder_fragment_button_monday);
+       tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+               if(isChecked){
+
+
+               }else{
+
+               }
+
+           }
+       });
+
+
+    }
+
+
 }
+
+
+
